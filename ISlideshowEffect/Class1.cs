d97dd7241cdf6b89e484c673214ae31b99e0f7d6
Yusif Alakbarzade <73228864+Redzlion@users.noreply.github.com>
2023-04-22 20:54:25 +0200

    Delete ISlideshowEffect directory

--- ISlideshowEffect/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows.Controls;
-
-public interface ISlideshowEffect
-{
-    string Name { get; }
-
-    void PlaySlideshow(Image imageIn, Image imageOut, double windowWidth, double windowHeight);
-
-}
